94787df6c517ac1ffa08e4235ecd8e7de933da03
rindoshingen <zenjukai@gmail.com>
2024-07-16 08:53:18 -0600

    HTMX Readling List

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import createHomepageTemplate from './views/index.js';
+import createListTemplate from './views/list.js';
+import BOOKS_DATA from './data/data.js';
+
+// create app
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+
+// static assets
+app.use(express.static('public'));
+
+// routes
+app.get('/', (req, res) => {
+	res.send(createHomepageTemplate());
+});
+
+app.get('/books', (req, res) => {
+	res.send(createListTemplate());
+});
+
+app.post('/books', (req, res) => {
+	const { title, author } = req.body;
+	const id = Math.random().toString();
+
+	BOOKS_DATA.push({ id, title, author });
+
+	res.send(`<li>${title}, ${author}</li>`)
+});
+
+// listen to port
+app.listen(3000, () => {
+	console.log('App listening on port 3000');
+});
---
+++ views/index.js
@@ -0,0 +1,42 @@
+const createHomepageTemplate = () => /*html*/`
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>My Reading List</title>
+      <script src="https://unpkg.com/htmx.org@1.9.12"></script>
+      <link rel="stylesheet" href="/styles.css">
+    </head>
+    <body>
+      <header>
+        <h1>My Reading List</h1>
+      </header>
+
+      <main>
+        <div class="book-list">
+          <button hx-get="/books" hx-target=".book-list">Show Books</button>
+        </div>
+
+        <div class="add-book-form">
+          <h2>What do you want to read?</h2>
+          <form>
+            <input
+              id="title"
+              name="title"
+              placeholder="title"
+              type="text"
+            />
+            <input
+              id="author"
+              name="author"
+              placeholder="author"
+              type="text"
+            />
+            <button hx-post="/books" hx-target=".book-list ul" hx-swap="beforeend">Add</button>
+          </form>
+        </div>
+      </main>
+    </body>
+  </html>
+`;
+
+export default createHomepageTemplate;
